9153397199c1f003a3ba067d53f73ed6a70f345c
jjeeccss <jeicogacusna24@gmail.com>
2024-09-14 14:46:45 +0800

    Inital commit

---
+++ variables.js
@@ -0,0 +1,8 @@
+let name ="Jecs";
+let whatDoYoWannaBecomeInYourLife = "A programmer";
+let gender = "Male";
+let twitterHandle = "none";
+console.log(name);
+console.log(whatDoYoWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
